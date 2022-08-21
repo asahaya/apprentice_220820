313064a3e368b8721185916f240dca0b5ac11d86
asahaya <asa7kadensony@gmail.com>
2022-08-21 17:31:00 +0900

    Adding ingredients

--- lib/recipe.dart
+++ lib/recipe.dart
@@ -2,36 +2,154 @@ class Recipe_Data {
   String label;
   String imageURL;
   // TODO: Add servings and ingredients here
+  int servings;
+  List<Ingredient> ingredients;
+
   Recipe_Data(
     this.label,
     this.imageURL,
+    this.servings,
+    this.ingredients,
   );
   // TODO; Add List<Recipe> here
   static List<Recipe_Data> samples = [
     Recipe_Data(
       'Spaghetti and Meatballs',
       'assets/2126711929_ef763de2b3_w.jpg',
+      4,
+      [
+        Ingredient(
+          1,
+          'box',
+          'Spaghetti',
+        ),
+        Ingredient(
+          4,
+          '',
+          'Frozen Meatballs',
+        ),
+        Ingredient(
+          0.5,
+          'jar',
+          'sauce',
+        ),
+      ],
     ),
     Recipe_Data(
       'Tomato Soup',
       'assets/27729023535_a57606c1be.jpg',
+      2,
+      [
+        Ingredient(
+          1,
+          'can',
+          'Tomato Soup',
+        ),
+      ],
     ),
     Recipe_Data(
       'Grilled Cheese',
       'assets/3187380632_5056654a19_b.jpg',
+      1,
+      [
+        Ingredient(
+          2,
+          'slices',
+          'Cheese',
+        ),
+        Ingredient(
+          2,
+          'slices',
+          'Bread',
+        ),
+      ],
     ),
     Recipe_Data(
       'Chocolate Chip Cookies',
       'assets/15992102771_b92f4cc00a_b.jpg',
+      24,
+      [
+        Ingredient(
+          4,
+          'cups',
+          'flour',
+        ),
+        Ingredient(
+          2,
+          'cups',
+          'sugar',
+        ),
+        Ingredient(
+          0.5,
+          'cups',
+          'chocolate chips',
+        ),
+      ],
     ),
     Recipe_Data(
       'Taco Salad',
       'assets/8533381643_a31a99e8a6_c.jpg',
+      1,
+      [
+        Ingredient(
+          4,
+          'oz',
+          'nachos',
+        ),
+        Ingredient(
+          3,
+          'oz',
+          'taco meat',
+        ),
+        Ingredient(
+          0.5,
+          'cup',
+          'cheese',
+        ),
+        Ingredient(
+          0.25,
+          'cup',
+          'chopped tomatoes',
+        ),
+      ],
     ),
     Recipe_Data(
       'Hawaiian Pizza',
       'assets/15452035777_294cefced5_c.jpg',
+      4,
+      [
+        Ingredient(
+          1,
+          'item',
+          'pizza',
+        ),
+        Ingredient(
+          1,
+          'cup',
+          'pineapple',
+        ),
+        Ingredient(
+          8,
+          'oz',
+          'ham',
+        ),
+      ],
     ),
   ];
 }
+
 // TODO: Add Ingredient() here
+//Ingredient=原材料名
+//quantity  =量
+//measure　=ものさし
+class Ingredient {
+  double quantity;
+  String measure;
+  String name;
+
+  Ingredient(
+    this.quantity,
+    this.measure,
+    this.name,
+  );
+}
